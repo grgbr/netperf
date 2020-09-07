e9ab03e3b00aad490dd413d817b09f17591d0dee
Gregor Boirie <gregor.boirie@free.fr>
2020-09-08 00:45:11 +0200

    regenerate configure for cross compiling using automake copy mode

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 aclocal -I src/missing/m4 --install --force \
-&& automake  --add-missing --force-missing \
+&& automake  --copy --add-missing --force-missing \
 && autoconf --force && autoheader --force
